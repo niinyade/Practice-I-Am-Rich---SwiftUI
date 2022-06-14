7613b23ae73363fc8579ace9aa5547354e880b34
Nyade <niinyade@icloud.com>
2022-06-14 16:17:33 -0300

    Initial Commit

---
+++ I Am Rich SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich SwiftUI
+//
+//  Created by Nyade on 14/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
